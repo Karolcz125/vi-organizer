3d7e8403824f97eaac75678d95e9d387f0ea98ba
karolcz125 <karolcz125@gmail.com>
2018-09-13 19:49:06 +0200

    Initial commit

---
+++ src/main/java/ru/vikopappcreatingteam/viorganizer/ViOrganizerApplication.java
@@ -0,0 +1,12 @@
+package ru.vikopappcreatingteam.viorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViOrganizerApplication.class, args);
+    }
+}
